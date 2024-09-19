41946b1e036982b35c29fa8e7e59ace9bc6e3013
jeke8989 <144659393+jeke8989@users.noreply.github.com>
2024-09-19 23:59:58 +0300

    save

--- api/api_v1/endpoints.py
+++ api/api_v1/endpoints.py
@@ -1,6 +1,6 @@
 import traceback
 from fastapi import APIRouter
-from core import get_info_list, get_single_job
+from core import get_info_list, get_single_job, event_job_subscription
 from loguru import logger
 
 router = APIRouter()
@@ -33,3 +33,7 @@ async def get_list_jobs_upwork(url: str, api_key: str):
         logger.exception(e)
         msg = f"error: {e.__class__.__name__}. message: {e}. traceback: {traceback.format_exc()}"
         return {"ok": False, "message": msg}
+    
+@router.post('/create_cycle_sub')
+async def cycle_sub(link_subs: str, version: str, api_key: str, host: str, endpoint: str = None):
+    await event_job_subscription(link_subs=link_subs, version=version, api_key=api_key, host=host, endpoint=endpoint)
